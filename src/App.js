d7c3b8e0502815b249c225b8ff1a3cf1912cc94c
Yusuf Nafey <yusufnafey@yahoo.com>
2019-02-26 21:54:25 -0800

    added layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import React, { Component } from "react";
 import "./App.scss";
+import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
 
 class App extends Component {
   render() {
-    return <div className="App">Hello world</div>;
+    return (
+      <div className="demo-big-content">
+        <Layout>
+          <Header title="Title" scroll>
+            <Navigation>
+              <a href="/">Link</a>
+              <a href="/">Link</a>
+              <a href="/">Link</a>
+              <a href="/">Link</a>
+            </Navigation>
+          </Header>
+          <Drawer title="Title">
+            <Navigation>
+              <a href="/">Link</a>
+              <a href="/">Link</a>
+              <a href="/">Link</a>
+              <a href="/">Link</a>
+            </Navigation>
+          </Drawer>
+          <Content>
+            <div className="page-content" />
+          </Content>
+        </Layout>
+      </div>
+    );
   }
 }
 

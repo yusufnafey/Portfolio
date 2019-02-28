276aa1a31947584637e5269bb4f8dff75f352a3a
Yusuf Nafey <yusufnafey@yahoo.com>
2019-02-27 22:24:46 -0800

    set up project tabs

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,10 +1,27 @@
 import React, { Component } from "react";
+import { Tabs, Tab } from "react-mdl";
 
 class Projects extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeTab: 0
+    };
+  }
+
   render() {
     return (
-      <div>
-        <h1>Projects Page</h1>
+      <div className="category-tabs">
+        <Tabs
+          activeTab={this.state.avtiveTab}
+          onChange={tabId => this.setState({ activeTab: tabId })}
+          ripple
+        >
+          <Tab>React</Tab>
+          <Tab>Angular</Tab>
+          <Tab>VueJS</Tab>
+          <Tab>MongoDB</Tab>
+        </Tabs>
       </div>
     );
   }

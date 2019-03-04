ae0319d448fcfa93e7c7cff8f2b47a2dc3d84b16
Yusuf Nafey <yusufnafey@yahoo.com>
2019-03-04 12:37:54 -0800

    started resume

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -1,10 +1,18 @@
 import React, { Component } from "react";
+import { Grid, Cell } from "react-mdl";
 
 class Resume extends Component {
   render() {
     return (
-      <div>
-        <h1>Resume Page</h1>
+      <div className="resume-body">
+        <Grid>
+          <Cell className="resume-left-col" col={4}>
+            Left Side
+          </Cell>
+          <Cell className="resume-right-col" col={8}>
+            Right Side
+          </Cell>
+        </Grid>
       </div>
     );
   }

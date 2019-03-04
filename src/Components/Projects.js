3cf8b35bfe883054eb8b8cc617b1b5a2017940ee
Yusuf Nafey <yusufnafey@yahoo.com>
2019-03-03 19:57:49 -0800

    started card

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,5 +1,17 @@
 import React, { Component } from "react";
-import { Tabs, Tab, Grid, Cell } from "react-mdl";
+import {
+  Tabs,
+  Tab,
+  Grid,
+  Cell,
+  Card,
+  CardTitle,
+  CardText,
+  CardActions,
+  Button,
+  CardMenu,
+  IconButton
+} from "react-mdl";
 
 class Projects extends Component {
   constructor(props) {
@@ -13,7 +25,37 @@ class Projects extends Component {
     if (this.state.activeTab === 0) {
       return (
         <div>
-          <h1>This is React</h1>
+          <Card shadow={5} style={{ minWidth: "450px", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "#fff",
+                height: "176px",
+                background:
+                  "url(https://cdn.worldvectorlogo.com/logos/react.svg) center / cover"
+              }}
+            >
+              <CardText>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. It has
+                survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged. It was
+                popularised in the 1960s with the release of Letraset sheets
+                containing Lorem Ipsum passages, and more recently with desktop
+                publishing software like Aldus PageMaker including versions of
+                Lorem Ipsum.
+              </CardText>
+              <CardActions border>
+                <Button colored>GitHub</Button>
+                <Button colored>CodePen</Button>
+                <Button colored>LiveDemo</Button>
+              </CardActions>
+              <CardMenu style={{ color: "#fff" }}>
+                <IconButton name="share" />
+              </CardMenu>
+            </CardTitle>
+          </Card>
         </div>
       );
     } else if (this.state.activeTab === 1) {
@@ -50,7 +92,13 @@ class Projects extends Component {
           <Tab>VueJS</Tab>
           <Tab>MongoDB</Tab>
         </Tabs>
-        <section className="projects-grid">{this.toggleCategories()}</section>
+        <section className="projects-grid">
+          <Grid className="projects-grid">
+            <Cell col={12}>
+              <div className="content">{this.toggleCategories()}</div>
+            </Cell>
+          </Grid>
+        </section>
       </div>
     );
   }

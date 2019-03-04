eba35b8aa732bd0754ce826cbb79a2ecf88173f7
Yusuf Nafey <yusufnafey@yahoo.com>
2019-03-03 21:04:17 -0800

    contact page left side complete

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -6,7 +6,19 @@ class Contact extends Component {
     return (
       <div className="contact-body">
         <Grid className="contact-grid">
-          <Cell col={6}>Half</Cell>
+          <Cell col={6}>
+            <h2>Yusuf Nafey</h2>
+            <img
+              src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png"
+              alt="avatar"
+            />
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </p>
+          </Cell>
           <Cell col={6}>Half</Cell>
         </Grid>
       </div>

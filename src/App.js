2a2943542dc8f026ba6cd50bb0c847b9dd0ad1a9
Yusuf Nafey <yusufnafey@yahoo.com>
2019-03-04 16:12:01 -0800

    fixed home button

--- src/App.js
+++ src/App.js
@@ -3,13 +3,22 @@ import "./App.scss";
 import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
 import Main from "./Components/Main";
 import { Link } from "react-router-dom";
+import { black } from "ansi-colors";
 
 class App extends Component {
   render() {
     return (
       <div className="demo-big-content">
         <Layout>
-          <Header className="header-color" title="Title" scroll>
+          <Header
+            className="header-color"
+            title={
+              <Link to="/" style={{ color: "white", textDecoration: "none" }}>
+                MyPortfolio
+              </Link>
+            }
+            scroll
+          >
             <Navigation>
               <Link to="/resume">Resume</Link>
               <Link to="/aboutme">About Me</Link>
@@ -17,7 +26,13 @@ class App extends Component {
               <Link to="/contact">Contact</Link>
             </Navigation>
           </Header>
-          <Drawer title="Title">
+          <Drawer
+            title={
+              <Link to="/" style={{ color: "black", textDecoration: "none" }}>
+                MyPortfolio
+              </Link>
+            }
+          >
             <Navigation>
               <Link to="/resume">Resume</Link>
               <Link to="/aboutme">About Me</Link>

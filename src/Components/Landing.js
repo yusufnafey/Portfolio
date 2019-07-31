b586fa8639dd4a76273438f5f0047ace0364f5ff
Yusuf Nafey <yusufnafey@yahoo.com>
2019-07-30 20:48:02 -0700

    installed bootstrap

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -7,11 +7,7 @@ class Landing extends Component {
       <div className="container">
         <Grid className="landing-grid">
           <Cell col={12}>
-            <img
-              src="https://www.shareicon.net/data/512x512/2017/02/05/878221_user_512x512.png"
-              alt="avatar"
-              className="avatar-img"
-            />
+            <img src="me.jpg" alt="avatar" className="avatar-img" />
             <div className="banner-text">
               <h1>Full Stack Web Developer</h1>
               <hr />
